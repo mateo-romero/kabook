6edb9e8f7f5b4d67e324180de2599b4b8e9dca2b
mateo-romero <mateoromeroweb@gmail.com>
2022-07-14 13:32:40 -0500

    maklend

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
-
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
@@ -13,10 +13,4 @@ app.get("/ping", function (req, res) {
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+app.listen(port);
