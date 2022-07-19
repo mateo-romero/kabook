146d2f5ed6a7138b5e4201254556b2f0bbb2e489
mateo-romero <mateoromeroweb@gmail.com>
2022-07-19 14:58:07 -0500

    maklend

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
-const express = require("express");
-const favicon = require("express-favicon");
-const path = require("path");
-const port = process.env.PORT || 8080;
-const app = express();
+var express = require("express");
+var app = express();
 
-app.use(favicon(__dirname + "/public/favicon.png"));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
+
+/* serves main page */
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(port);
+
+app.post("/user/add", function (req, res) {
+  /* some server side logic */
+  res.send("OK");
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function (req, res) {
+  console.log("static file request : " + req.params);
+  res.sendfile(__dirname + req.params[0]);
+});
+
+app.get("/favicon.ico", function (req, res) {
+  res.send(204);
+});
+
+var port = process.env.PORT || 5003;
+app.listen(port, function () {
+  console.log("Listening on " + port);
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
