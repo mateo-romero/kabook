3527d5931cecc1945e88fe7e0b3be388cd8c064c
mateo-romero <mateoromeroweb@gmail.com>
2022-07-19 14:19:21 -0500

    maklend

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(favicon(__dirname + "/public/favicon.png"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
